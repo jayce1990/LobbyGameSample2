6651e6226e1c73b399ae1f416896b1dfe31fc1e3
rtlong <rtlong@wooduan.com>
2024-09-09 11:33:20 +0800

    JoinCreateLobbyUI

---
+++ Assets/Scripts/UI/JoinCreateLobbyUI.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum EnumJoinCreateTabs
+{
+    Join,
+    Create
+}
+
+public class JoinCreateLobbyUI : UIPanelBase
+{
+    [HideInInspector]
+    public UnityEvent<EnumJoinCreateTabs> m_OnTabChanged;
+
+    [SerializeField]
+    EnumJoinCreateTabs m_CurrentTab = EnumJoinCreateTabs.Join;
+
+    EnumJoinCreateTabs CurrentTab
+    {
+        get => m_CurrentTab;
+        set
+        {
+            m_CurrentTab = value;
+            m_OnTabChanged?.Invoke(m_CurrentTab);
+        }
+    }
+
+    public override void Start()
+    {
+        base.Start();
+        Manager.onGameStateChanged += GameStateChanged;
+    }
+
+    void GameStateChanged(GameState state)
+    {
+        if (state == GameState.JoinMenu)
+        {
+            m_OnTabChanged?.Invoke(m_CurrentTab);//初始化:将之前的选择Tab,通知Join和Create进行显示和隐藏.
+
+            Show(false);
+        }
+        else
+        {
+            Hide();
+        }
+    }
+
+    public void SetJoinTab()//UI操作:切换Tab页,通知Join和Create进行显示和隐藏.
+    {
+        CurrentTab = EnumJoinCreateTabs.Join;
+    }
+
+    public void SetCreateTab()//同上
+    {
+        CurrentTab = EnumJoinCreateTabs.Create;
+    }
+
+    private void OnDestroy()
+    {
+        Manager.onGameStateChanged -= GameStateChanged;
+    }
+}
